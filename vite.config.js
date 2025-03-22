b6ccad2ff9fe4b8d65f88fcfae06ba3fdde740c6
princetonhuaren <axgong@outlook.com>
2025-03-21 21:46:04 -0400

    update path

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/princetonhuaren.github.io/",
+  base: "./",
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",
-  },
-  resolve: {
-    alias: {
-      src: "/src",
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-      },
-    },
-  },
+  // esbuild: {
+  //   loader: "jsx",
+  // },
+  // resolve: {
+  //   alias: {
+  //     src: "/src",
+  //     "./runtimeConfig": "./runtimeConfig.browser",
+  //   },
+  // },
+  // optimizeDeps: {
+  //   esbuildOptions: {
+  //     loader: {
+  //       ".js": "jsx",
+  //     },
+  //   },
+  // },
 });
