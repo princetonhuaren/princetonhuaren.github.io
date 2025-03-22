870b92978e93e6c0efe8a476e77f838cdf3c1dd5
princetonhuaren <axgong@outlook.com>
2025-03-21 22:13:38 -0400

    update base name to absolute path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./princetonhuaren.github.io/",
+  base: "/princetonhuaren.github.io/",
   plugins: [react()],
   // esbuild: {
   //   loader: "jsx",
